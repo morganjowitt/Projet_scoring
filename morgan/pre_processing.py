fb5ffc2a55c36a47964c03eff40b027433223441
morganjowitt <jowitt.morgan@gmail.com>
2024-10-10 16:47:56 +0200

    oubli

--- morgan/pre_processing.py
+++ morgan/pre_processing.py
@@ -29,22 +29,3 @@ class Preprocessing :
         else :
             print('méthode non existante')
         return data
-    
-    def entraîner_modele_et_predire_reg_log(self, data : pd.DataFrame, var_cible : str) -> None :
-
-        X = data.drop(columns=[var_cible])
-        y = data[var_cible]
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-        logistic_model = LogisticRegression(max_iter=1000)
-        logistic_model.fit(X_train, y_train)
-        y_pred = logistic_model.predict(X_test)
-
-        accuracy = accuracy_score(y_test, y_pred)
-        conf_matrix = confusion_matrix(y_test, y_pred)
-        class_report = classification_report(y_test, y_pred)
-        print(f"Accuracy: {accuracy}")
-        print("Confusion Matrix:")
-        print(conf_matrix)
-        print("Classification Report:")
-        print(class_report)
-
